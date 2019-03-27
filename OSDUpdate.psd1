2f31729e1cea1b7205dfb814136d196bc1925352
David Segura <david@segura.org>
2019-03-26 23:46:37 -0500

    19.3.26.0

--- OSDUpdate.psd1
+++ OSDUpdate.psd1
@@ -7,7 +7,7 @@
 RootModule = 'OSDUpdate.psm1'
 
 # Version number of his module.
-ModuleVersion = '19.3.21.0'
+ModuleVersion = '19.3.26.0'
 
 # Supported PSEditions
 # CompatiblePSEditions = @()
--- OSDUpdate.psm1
+++ OSDUpdate.psm1
@@ -1,10 +1,14 @@
-#https://github.com/RamblingCookieMonster/PSStackExchange/blob/master/PSStackExchange/PSStackExchange.psm1
-$PublicFunctions  = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue )
-$PrivateFunctions = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue )
+#===================================================================================================
+#   Import Functions
+#   https://github.com/RamblingCookieMonster/PSStackExchange/blob/master/PSStackExchange/PSStackExchange.psm1
+#===================================================================================================
+$OSDPublicFunctions  = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue )
+$OSDPrivateFunctions = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue )
 
-foreach ($Import in @($PublicFunctions + $PrivateFunctions)) {
+foreach ($Import in @($OSDPublicFunctions + $OSDPrivateFunctions)) {
     Try {. $Import.FullName}
     Catch {Write-Error -Message "Failed to import function $($Import.FullName): $_"}
 }
 
-Export-ModuleMember -Function $PublicFunctions.BaseName
+Export-ModuleMember -Function $OSDPublicFunctions.BaseName
+#===================================================================================================
